f3a7102b30db2de0d6dc05ecb56a88605029fe99
Elang <prokointech@gmail.com>
2025-04-27 11:27:12 +0700

    production

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/',
-  base: '/link-tree',
+  base: '/',
+  // base: '/link-tree',
   plugins: [vue(), tailwindcss()],
   resolve:{
     alias:{
